c6ec244e06be39da268aceae010fd40f8e3f7c7e
shubh <shubhamfargade99@gmail.com>
2020-07-20 01:25:59 +0530

    atlas

--- src/connection.js
+++ src/connection.js
@@ -1,9 +1,11 @@
   
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
+const MONGODB_URI = "mongodb+srv://siddharth-hotel:siddharth-hotel@cluster0.uye3i.gcp.mongodb.net/hotelmanagement?retryWrites=true&w=majority";
 
-mongoose.connect("mongodb://localhost:27017/hotelmanagement",{
-    useCreateIndex: true,
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
-})
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB Connected…");
+}).catch(err => console.log(err))
