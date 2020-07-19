80c6a56cf440b499a0e3b4cda90cb8488b61f5c5
shubh <shubhamfargade99@gmail.com>
2020-07-20 03:11:35 +0530

    s

--- src/connection.js
+++ src/connection.js
@@ -10,7 +10,7 @@
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://siddharth-hotel:siddharth@cluster0.uye3i.gcp.mongodb.net/hotelmanagement?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology:true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
